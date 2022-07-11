8312a00f2e189e71eb73d106bc3feb9e4b5f1995
Ruslan <sasa02sasa@mail.ru>
2022-07-11 13:27:29 +0600

    Сделано мобильное меню и селекты

--- js/script.js
+++ js/script.js
@@ -10,3 +10,28 @@ faqElements.forEach((element) => {
     }
   });
 });
+
+const navbarOpenElements = document.querySelectorAll(".navbar-open");
+
+navbarOpenElements.forEach((element) => {
+  element.addEventListener("click", () => {
+    const navbarShow = element.querySelector(".navbar-show");
+    if (navbarShow.classList.contains("navbar-close")) {
+      navbarShow.classList.remove("navbar-close");
+    } else {
+      navbarShow.classList.add("navbar-close");
+    }
+  });
+});
+
+const buttonOpenMobileMenu = document.querySelector(".header-mobile-icon__button");
+buttonOpenMobileMenu.addEventListener("click", () => {
+  const mobileMenu = document.querySelector(".header-mobile-menu");
+  if (mobileMenu.style.height) {
+    mobileMenu.style.height = null;
+    document.querySelector("body").style.overflow = "auto";
+  } else {
+    document.querySelector("body").style.overflow = "hidden";
+    mobileMenu.style.height = window.innerHeight + "px";
+  }
+});
